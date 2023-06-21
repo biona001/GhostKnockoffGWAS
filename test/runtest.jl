9160e273d14188389b423fb72c84248488d30da3
Unknown <benchu99@hotmail.com>
2023-06-21 13:03:08 -0700

    add some tests

--- test/runtest.jl
+++ test/runtest.jl
@@ -1,3 +1,8 @@
 @testset "utilities" begin
-    @test 1 == 1
+    # convert between z score and p-value
+    p = 0.00695184371278
+    beta = -0.010602
+    z = 2.6991423324213346
+    @test zscore(p, beta) ≈ 2.6991423324213346
+    @test pval(z) ≈ 0.00695184371278
 end
