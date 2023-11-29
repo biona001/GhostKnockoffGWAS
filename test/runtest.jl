72ccd5113cfdf605aa8862ded75b5883913f0ce2
Benjamin Chu <benchu99@hotmail.com>
2023-11-29 09:58:29 -0800

    add a test to LD shrinkage

--- test/runtest.jl
+++ test/runtest.jl
@@ -13,3 +13,22 @@
     @test c == [[2], [1, 3], [5], [1, 3], Int[]]
     @test typeof(c) <: Vector{Vector{Int64}}
 end
+
+@testset "LD shrinkage" begin
+    function mvn_logl_under_null_naive(Σ::AbstractMatrix, z::AbstractVector)
+        return -0.5logdet(Symmetric(Σ)) - dot(z, inv(Symmetric(Σ)), z)
+    end
+
+    p = 1000
+    z = randn(p)
+    x = randn(p, p)
+    storage=zeros(length(z))
+    Sigma = Symmetric(x'*x)
+    L = cholesky(Symmetric(Sigma));
+    a = mvn_logl_under_null_naive(Sigma, z)
+    b = mvn_logl_under_null(L, z)
+    @test a ≈ b
+
+    γ = find_optimal_shrinkage(Sigma, z)
+    @test 0 ≤ γ ≤ 1
+end
