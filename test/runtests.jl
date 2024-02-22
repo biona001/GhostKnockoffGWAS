ffc264dc9b61550a092d96fae3e5a90f9c43b96f
Benjamin Chu <benchu99@hotmail.com>
2024-02-22 10:02:06 -0800

    avoid testing Ghostbasil.jl code in GhostKnockoffGWAS

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,41 +40,42 @@ end
     @test isapprox(γ, γtrue, atol=1e-12)
 end
 
-@testset "ghostbasil C++ solver" begin
-    # basic test to make sure block_group_ghostbasil does not crash
-    p = 10 # number of features
-    m = 5  # number of knockoffs
-    x = randn(p, p)
-    y = randn(p, p)
-    Ci = x'*x
-    Si = y'*y
-    r = randn(6p)
-    lambda_path = [0.1, 0.05, 0.01, 0.001]
-    beta_i = block_group_ghostbasil(Ci, Si, r, lambda_path, m=m)
-    @test length(beta_i) == (m+1)*p
+# commented out since more extensive test of this is in Ghostbasil.jl
+# @testset "ghostbasil C++ solver" begin
+#     # basic test to make sure block_group_ghostbasil does not crash
+#     p = 10 # number of features
+#     m = 5  # number of knockoffs
+#     x = randn(p, p)
+#     y = randn(p, p)
+#     Ci = x'*x
+#     Si = y'*y
+#     r = randn(6p)
+#     lambda_path = [0.1, 0.05, 0.01, 0.001]
+#     beta_i = block_group_ghostbasil(Ci, Si, r, lambda_path, m=m)
+#     @test length(beta_i) == (m+1)*p
 
-    # Test correctness with a specific region from Pan-UKBB. 
-    # The true answer is obtained from running the ghostbasil R package on
-    # the given data. Note these data are based on LD summary statistics, 
-    # which can be freely distributed
-    datadir = normpath(Ghostbasil.datadir())
-    C = readdlm(joinpath(datadir, "C.txt"))
-    S = readdlm(joinpath(datadir, "S.txt"))
-    r = readdlm(joinpath(datadir, "r.txt")) |> vec
-    lambda_path = readdlm(joinpath(datadir, "lambda_path.txt")) |> vec
-    beta_i = block_group_ghostbasil(C, S, r, lambda_path)
-    @test count(!iszero, beta_i) == 14
-    non_zero_idx = findall(!iszero, beta_i)
-    @test non_zero_idx == [215, 340, 341, 431, 559, 560, 763, 878,
-        1085, 1528, 1770, 2726, 3157, 3387]
-    @test all(beta_i[non_zero_idx] .≈ 
-        [-0.0005451576807477974, -0.0002947665358369119, -0.014810108231841996, 
-        -0.00043238505905553924, -0.006856782960094195, 0.0016850133196618926, 
-        -0.00019274743491070077, -0.0003841629682186662, -0.0005758897376517483, 
-        -0.0016095272344540995, 0.0020146776266541312, -0.001666736964454875, 
-        0.0004269118551173737, 0.00010746291189108347]
-    )
-end
+#     # Test correctness with a specific region from Pan-UKBB. 
+#     # The true answer is obtained from running the ghostbasil R package on
+#     # the given data. Note these data are based on LD summary statistics, 
+#     # which can be freely distributed
+#     datadir = normpath(Ghostbasil.datadir())
+#     C = readdlm(joinpath(datadir, "C.txt"))
+#     S = readdlm(joinpath(datadir, "S.txt"))
+#     r = readdlm(joinpath(datadir, "r.txt")) |> vec
+#     lambda_path = readdlm(joinpath(datadir, "lambda_path.txt")) |> vec
+#     beta_i = block_group_ghostbasil(C, S, r, lambda_path)
+#     @test count(!iszero, beta_i) == 14
+#     non_zero_idx = findall(!iszero, beta_i)
+#     @test non_zero_idx == [215, 340, 341, 431, 559, 560, 763, 878,
+#         1085, 1528, 1770, 2726, 3157, 3387]
+#     @test all(beta_i[non_zero_idx] .≈ 
+#         [-0.0005451576807477974, -0.0002947665358369119, -0.014810108231841996, 
+#         -0.00043238505905553924, -0.006856782960094195, 0.0016850133196618926, 
+#         -0.00019274743491070077, -0.0003841629682186662, -0.0005758897376517483, 
+#         -0.0016095272344540995, 0.0020146776266541312, -0.001666736964454875, 
+#         0.0004269118551173737, 0.00010746291189108347]
+#     )
+# end
 
 @testset "read_zscores" begin
     # no missing data
