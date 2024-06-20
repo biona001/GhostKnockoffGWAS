ee2f624c486b383b12a9739d3eeefec27e2f549d
Benjamin Chu <benchu99@hotmail.com>
2024-06-20 12:10:41 -0700

    improve efficiency, output SNP info in dataframe

--- src/make_hdf5.jl
+++ src/make_hdf5.jl
@@ -31,25 +31,25 @@ double precision numeric matrix.
 """
 function get_block(
     vcffile::String, 
-    chr::Union{String, Int}, 
-    start_bp::Union{String, Int}, 
-    end_bp::Union{String, Int};
+    chr::String, 
+    start_bp::Int, 
+    end_bp::Int;
     min_maf::Float64=0.01,
     min_hwe::Float64=0.0
     )
-    chr = string(chr)
-    start_bp = Int(start_bp)
-    end_bp = Int(end_bp)
-
     reader = VCF.Reader(openvcf(vcffile, "r"))
     T = Float64
     n = nsamples(vcffile)
     nsnps = 0
     X = ElasticArray{T}(undef, n, 0)
-    chrs, poss, refs, alts = String[], Int[], String[], String[]
+    df = DataFrame("rsid"=>String[], "AF"=>T[], "chr"=>String[], "pos"=> Int[], 
+                   "ref"=> String[], "alt"=>String[])
     model = :additive # additive genetic model
 
-    for record in reader
+    record = VCF.Record()
+    while !eof(reader)
+        read!(reader, record)
+
         # check chr
         chr_i = VCF.chrom(record)
         chr_i > chr && break
@@ -84,30 +84,27 @@ function get_block(
         end
 
         # save record info
-        push!(chrs, chr_i)
-        push!(poss, pos_i)
-        push!(refs, VCF.ref(record))
-        push!(alts, alt_i[1])
+        push!(df, [join(VCF.id(record), ','), alt_freq, chr_i, pos_i, 
+                  VCF.ref(record), alt_i[1]])
     end
 
-    return Matrix(X), chrs, poss, refs, alts
+    return Matrix(X), df
 end
-# using VCFTools, VariantCallFormat, ElasticArrays
+# using VCFTools, VariantCallFormat, ElasticArrays, DataFrames
 # vcffile = "/oak/stanford/groups/zihuai/paisa/chr1.vcf.gz"
-# chr = 1 
+# chr = "1"
 # min_maf = 0.0
 # min_hwe = 0.0
 # start_bp = 58814 # first 10 records
 # end_bp = 801536
-# @time X, chr, pos, ref, alt = get_block(vcffile, chr, start_bp, end_bp, min_maf=min_maf, min_hwe=min_hwe)
-# 0.014761 seconds (60.13 k allocations: 18.505 MiB, 9.10% gc time)
+# @time X, df = get_block(vcffile, chr, start_bp, end_bp, min_maf=min_maf, min_hwe=min_hwe)
+# 0.012035 seconds (46.78 k allocations: 13.440 MiB)
 
-# chr = 1 
+# chr = "1"
 # start_bp = 249208612 # last 10 records
 # end_bp = 249222527
-# @time X, chr, pos, ref, alt = get_block(vcffile, chr, start_bp, end_bp, min_maf=min_maf, min_hwe=min_hwe)
-# 54.847166 seconds (263.11 M allocations: 78.248 GiB, 2.47% gc time)
-
+# @time X, df = get_block(vcffile, chr, start_bp, end_bp, min_maf=min_maf, min_hwe=min_hwe)
+# 44.146655 seconds (196.81 M allocations: 53.676 GiB, 2.20% gc time)
 
 function validate(record, alt_i)
     if VariantCallFormat.findgenokey(record, "GT") === nothing
