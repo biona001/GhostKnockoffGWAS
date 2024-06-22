46de9d7f2171d6adf0109cb7ad5cae9984d62483
Benjamin Chu <benchu99@hotmail.com>
2024-06-22 16:14:15 -0700

    update toml

--- src/GhostKnockoffGWAS.jl
+++ src/GhostKnockoffGWAS.jl
@@ -13,11 +13,19 @@ using ArgParse
 using Statistics
 using StatsBase
 
+# needed for solve_blocks()
+using Knockoffs
+using VCFTools
+using VariantCallFormat
+using ElasticArrays
+
 export ghostknockoffgwas,
-    read_zscores
+    read_zscores,
+    solve_blocks
 
 include("ghostbasil_parallel.jl")
 include("utilities.jl")
 include("app.jl")
+include("make_hdf5.jl")
 
 end # module GhostKnockoffGWAS
